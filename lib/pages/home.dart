189f734a9a32b37c618e926620940cb687022636
Thomas Clark <tnc1997@virginmedia.com>
2018-03-15 16:01:44 +0000

    feat: add welcome text, image and icons to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,11 +6,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Column _buildIconColumn(IconData icon, String label) {
+    return new Column(
+      children: <Widget>[
+        new CircleAvatar(
+          child: new Icon(
+            icon,
+            size: 40.0,
+          ),
+          radius: 30.0,
+        ),
+        new Container(
+          child: new Text(
+            label,
+            style: new TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+          padding: new EdgeInsets.only(top: 8.0),
+        ),
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Avon Farm Foods'),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new Image.asset(
+            'images/home.jpg',
+            fit: BoxFit.cover,
+          ),
+          new Container(
+            child: new Text(
+              'Welcome to Avon Farm Foods',
+              style: new TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            padding: new EdgeInsets.only(bottom: 32.0, top: 32.0),
+          ),
+          new Container(
+            child: new Row(
+              children: <Widget>[
+                _buildIconColumn(Icons.shopping_basket, 'Shopped'),
+                _buildIconColumn(Icons.credit_card, 'Secured'),
+                _buildIconColumn(Icons.local_shipping, 'Shipped'),
+                _buildIconColumn(Icons.sentiment_very_satisfied, 'Satisfied'),
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            ),
+          ),
+        ],
+        padding: new EdgeInsets.all(16.0),
       ),
     );
   }
